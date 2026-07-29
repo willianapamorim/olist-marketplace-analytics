4c489b824da7d6d062d1abc3585ea3d7cb229f61
willianapamorim <willianapamorim@gmail.com>
2026-07-29 17:52:21 -0300

    fix(ops): corrige nome da funcao no run_bronze

--- src/01_bronze/run_bronze.py
+++ src/01_bronze/run_bronze.py
@@ -6,8 +6,8 @@ import sys
 sys.path.insert(0, "/Workspace/Users/willianapamorim@gmail.com/olist-marketplace-analytics/src/99_utils")
 sys.path.insert(0, "/Workspace/Users/willianapamorim@gmail.com/olist-marketplace-analytics/src/01_bronze")
 
-from ingest_bronze import ingest_bronze
+from ingest_bronze import run_bronze_ingestion
 from bronze_quality_checks import run_bronze_quality_checks
 
-ingest_bronze(spark)
+run_bronze_ingestion(spark)
 run_bronze_quality_checks(spark)
